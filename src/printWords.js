0c092aef0b19797f3a6dde3bece7362a890acb00
Kevin Chen <kc3585@nyu.edu>
2019-06-03 17:35:23 -0400

    WIP: changes to print words

---
+++ src/printWords.js
@@ -0,0 +1,5 @@
+export default printWords
+
+function printWords (word){
+  return word
+}
---
+++ test/printWords.test.js
@@ -0,0 +1,9 @@
+import test from 'ava'
+import {printWords} from '../src'
+
+test('print correct words', t => {
+  const original = "word"
+  const expected = word
+  const actual = printWords(original)
+  t.deepEqual(acutal, expected)
+})
